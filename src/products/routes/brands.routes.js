b6e093bd16f49712bd776e7da8866a38d7a53536
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-12 18:02:24 -0500

    asignacion de dto brand a la ruta

--- src/products/routes/brands.routes.js
+++ src/products/routes/brands.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 // dtos
-const brandSchema = require("../dto/brand.dto");
+const brandDto = require("../dto/brand.dto");
 // middleware
 const validationSchema = require("../../utils/middleware/validate-schema");
 // services
@@ -11,7 +11,7 @@ const bransServices = new BrandsServices();
 
 router.get("/", bransServices.getBrands);
 router.get("/:id", bransServices.getBrand);
-router.post("/", brandSchema, validationSchema, bransServices.createBrand);
+router.post("/", brandDto, validationSchema, bransServices.createBrand);
 router.put("/:id", bransServices.updateBrand);
 router.delete("/:id", bransServices.deleteBrand);
 
