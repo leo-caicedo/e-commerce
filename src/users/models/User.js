27628b565648ddbd41ecb2823e1d62a06c7f6005
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-13 16:22:43 -0500

    User Model

---
+++ src/users/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    country: String,
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+  },
+  {
+    timestamp: true,
+    versionKey: false,
+  }
+);
+
+// encrypt password
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(6);
+  return await bcrypt.hash(password, salt);
+};
+
+// delete password for response
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    delete returnedObject.password;
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
