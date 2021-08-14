508859828f69be546bb801f30aa64badcd92bdc7
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-13 21:17:21 -0500

    servicios de crude para user

--- src/users/services/user.service.js
+++ src/users/services/user.service.js
@@ -4,6 +4,51 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 class UserServices {
+  async getUsers(req, res, next) {
+    try {
+      const users = await User.find({});
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getUser(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findById(id);
+      res.json(use);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async updateUser(req, res, next) {
+    const { id } = req.params;
+    const { body: data } = req;
+
+    try {
+      const userUpdated = await User.findOneAndUpdate(id, data, { new: true });
+      await userUpdated.save();
+
+      res.json(userUpdated);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async deleteUser(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await User.findByIdAndDelete(id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+
   // auth
   async singUp(req, res, next) {
     const { username, password, country, orders } = req.body;
