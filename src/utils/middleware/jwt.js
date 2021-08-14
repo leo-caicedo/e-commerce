0bd6e95ec8f1dbc8842934e38088f0884a8d5244
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-14 17:02:47 -0500

    middleware de jwt

---
+++ src/utils/middleware/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const { config } = require("../../config");
+// models
+const User = require("../../users/models/User");
+
+const verifyToken = async (req, res, next) => {
+  const token = req.haaders["access-token"];
+  if (!token) {
+    return res.status(403).json({ message: "no token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.SECRET);
+    req.userId = decoded.id;
+    const user = await User.findById(req.userId, { password: 0 });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = verifyToken;
