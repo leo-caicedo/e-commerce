20387288c7adb3df85bacb70335bde0c017342a4
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-13 20:25:07 -0500

    username debe ser unico

--- src/users/dto/user.dto.js
+++ src/users/dto/user.dto.js
@@ -1,12 +1,22 @@
 const { body } = require("express-validator");
 
+// models
+const User = require("../models/User");
+
 const userDto = [
   // username
   body("username")
     .exists({ checkFalsy: false })
     .withMessage("Username is required")
     .isLength({ max: 30 })
-    .withMessage("The username must have a maximum of 30 characters"),
+    .withMessage("The username must have a maximum of 30 characters")
+    .custom((username) => {
+      return User.findOne({ username }).then((user) => {
+        if (user) {
+          return Promise.reject("Username already in use");
+        }
+      });
+    }),
   // password
   body("password")
     .exists({ checkFalsy: false })
